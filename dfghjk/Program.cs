18b6f43a55fba1d40968e9cb026c84fa20a96229
villehvo <89245411+villehvo@users.noreply.github.com>
2022-02-18 09:54:21 +0100

    Add files via upload

---
+++ dfghjk/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace dfghjk
+{
+   class MySort 
+   {
+      static void Main(string[] args) {
+
+          Stopwatch stop = Stopwatch.StartNew();
+          Thread.Sleep(1000);
+          stop.Restart();
+          Thread.Sleep(1000);
+
+
+        Random rnd = new Random();
+         int[] arr = new int[1000];
+          for(int j = 0; j < 1000; j++)
+          {
+              arr[j]=(rnd.Next(1, 1000));
+          }
+          
+           
+
+         
+         int temp;
+         for (int j = 0; j <= arr.Length - 2; j++) {
+            for (int i = 0; i <= arr.Length - 2; i++) {
+               if (arr[i] > arr[i + 1]) {
+                  temp= arr[i + 1];
+                  arr[i + 1] = arr[i];
+                  arr[i] = temp;
+               }
+            }
+         }
+         
+         stop.Stop();
+         Console.WriteLine("done");
+         foreach (int p in arr)
+            Console.Write(p + " ");
+            Console.WriteLine("Time = " + stop.ElapsedMilliseconds);
+          Console.Read();
+      }
+   }
+}
